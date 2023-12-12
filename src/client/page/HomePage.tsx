dfd2042624f9d23648fa7b37356e5103a72c5b80
Nick Cherry <nickcherryjiggz@gmail.com>
2023-12-11 19:11:46 -0500

    update home page title

--- src/client/page/HomePage.tsx
+++ src/client/page/HomePage.tsx
@@ -15,7 +15,7 @@ function Paragraph({ children }: { children: ReactNode }) {
 
 function HomePage() {
   return (
-    <Page title="Home" className="min-h-[1500px]">
+    <Page title="Nick.js" className="min-h-[1500px]">
       <Section>
         <Heading>Overview</Heading>
         <Paragraph>
